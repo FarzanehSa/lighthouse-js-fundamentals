66eb8a384b6d131ad199b1f4205e759f719c2c67
FarzanehSadegh <farzanehsadegh@gmail.com>
2021-12-28 14:44:46 -0800

    <we add array examples>

---
+++ ice-cream.js
@@ -0,0 +1,11 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push('root beer');
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+let i = iceCreamFlavours.length;
+console.log(iceCreamFlavours[i-1]);
+console.log(i);
